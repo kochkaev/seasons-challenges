6669f5a77c8a37d4ea557dd1b30e250b5ac24c9e
kochkaev <edukisel@yandex.ru>
2024-09-02 15:10:34 +0700

    Updated ModMenu integration. Seasons API version was updated to 1.0-RC-4.3.

---
+++ src/client/java/ru/kochkaev/seasons/integration/mod/ModMenu.java
@@ -0,0 +1,23 @@
+package ru.kochkaev.seasons.integration.mod;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.minecraft.client.gui.screen.Screen;
+import ru.kochkaev.api.seasons.SeasonsAPI;
+import ru.kochkaev.api.seasons.integration.mod.ClothConfig;
+import ru.kochkaev.api.seasons.service.Config;
+
+public class ModMenu implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> {
+            if (SeasonsAPI.getClothConfig()!=null) {
+                return (Screen) ClothConfig.getClient().getConfigScreen(parent, Config.getModConfig("Seasons Challenges"));
+                // Return the screen here with the one you created from Cloth Config Builder
+            }
+            return null;
+        };
+    }
+
+}
